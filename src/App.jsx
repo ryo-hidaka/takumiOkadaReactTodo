a09c255322cbe504ee4b7814a156046fbe762dc8
ryo-hidaka <66785120+ryo-hidaka@users.noreply.github.com>
2022-05-17 17:05:07 +0900

    スタイル当て

--- src/App.jsx
+++ src/App.jsx
@@ -2,5 +2,40 @@ import React from "react";
 import "./styles.css";
 
 export const App = () => {
-  return <div></div>;
+  return (
+    <>
+      <div className="input-area">
+        <input placeholder="TODOを入力" />
+        <button>追加</button>
+      </div>
+      <div className="incomplete-area">
+        <p className="title">未完了のTODO</p>
+        <ul>
+          <li>
+            <div className="list-row">
+              <li>aaa</li>
+              <button className>完了</button>
+              <button className>削除</button>
+            </div>
+            <div className="list-row">
+              <li>aaa</li>
+              <button className>完了</button>
+              <button className>削除</button>
+            </div>
+          </li>
+        </ul>
+      </div>
+      <div className="complete-area">
+        <p className="title">完了のTODO</p>
+        <ul>
+          <li>
+            <div className="list-row">
+              <li>aaa</li>
+              <button className>戻す</button>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </>
+  );
 };
